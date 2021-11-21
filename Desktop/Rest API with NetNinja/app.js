eef495e18b0a67096ef781d3b96a4de1e7ed7507
Flamanyoman <94542902+Flamanyoman@users.noreply.github.com>
2021-11-21 22:15:41 +0100

    Update app.js

--- Desktop/Rest API with NetNinja/app.js
+++ Desktop/Rest API with NetNinja/app.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 
 const routes = require('./routes/routes');
 
-const dbURI =
-  'mongodb+srv://flaboy:flaboy123@acadera.hqkcv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
     const PORT = process.env.PORT || 3000;
